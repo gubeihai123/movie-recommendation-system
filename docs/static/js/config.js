0dbd1e12498aed04495c0772f45e3444de510fee
gubeihai123 <2700035833@qq.com>
2026-06-10 20:15:11 +0800

    Point GitHub Pages frontend to Render API

--- docs/static/js/config.js
+++ docs/static/js/config.js
@@ -1,2 +1,2 @@
 // GitHub Pages 前端配置：部署 Render 后端后，把这里改成你的后端公网地址。
-window.MOVIE_API_BASE = "https://YOUR_RENDER_SERVICE.onrender.com";
+window.MOVIE_API_BASE = "https://movie-recommendation-system-68a3.onrender.com";
